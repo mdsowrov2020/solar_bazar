63d4a0093914ebabbc4fb65720b420216ca00b3a
Antu <prince.anto97@gmai.com>
2021-02-05 23:25:04 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,45 @@
+function toggleCart() {
+  var element = document.getElementById('custom_drop_item_id');
+  element.classList.toggle('custom_drop_item_class');
+}
+
+// Latest Slider
+$('.new_slider').slick({
+  infinite: true,
+  slidesToShow: 5,
+  centerMode: true,
+  prevArrow:
+    '<span class="prev_arrow_latest"><i class="fa fa-chevron-left" aria-hidden="true"></i></span>',
+  nextArrow:
+    '<span class="next_arrow_latest"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
